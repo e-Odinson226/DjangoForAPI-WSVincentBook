7b0af877ab1dd22328df3d9ca1395e59e32714d1
zakaria <erfan.yekezare@gmail.com>
2022-09-03 12:10:03 +0430

    install whitenoise package and config settings to use whitenoise as storage and staticfiles handler add runtime.txt

--- conifg/settings.py
+++ conifg/settings.py
@@ -42,11 +42,13 @@ INSTALLED_APPS = [
     "apis.apps.ApisConfig",
     # 3rd Party
     "rest_framework",
+    "whitenoise.runserver_nostatic",
 ]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -121,7 +123,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
