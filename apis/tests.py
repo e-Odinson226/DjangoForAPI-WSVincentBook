2dfc0d55b74f79dcfc8c85940daa7dd8f84e27a9
zakaria <erfan.yekezare@gmail.com>
2022-09-03 11:57:32 +0430

    write test on api routing, context.

--- apis/tests.py
+++ apis/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from rest_framework import status
+from django.urls import reverse
 
-# Create your tests here.
+from books.models import Book
+
+
+class APITest(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.book = Book.objects.create(
+            title="test book title",
+            subtitle="test book subtitle",
+            author="test book author",
+            isbn="test book isbn",
+        )
+
+    def test_api_list_view(self):
+        response = self.client.get(reverse("book_list_api"))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(Book.objects.count(), 1)
+        self.assertContains(response, self.book)
